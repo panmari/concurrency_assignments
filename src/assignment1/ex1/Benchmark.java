73876bd5de72640e71bb3cbace793233cd20c6d1
panmari <smmuzi@gmail.com>
2015-02-26 13:51:36 +0100

    Also computing speedup with unlocking version as reference.

--- src/assignment1/ex1/Benchmark.java
+++ src/assignment1/ex1/Benchmark.java
@@ -10,18 +10,26 @@ public class Benchmark {
         int[] different_nr_threads = new int[] {1, 2, 4, 8};
         for (int nr_threads: different_nr_threads) {
             long runtime = 0L;
+            long[] runtimes = new long[ComputationMode.values().length];
             for (ComputationMode cm: ComputationMode.values()) {
-                // Manually call gc before benchmarking.
                 for (int i = 0; i < NR_RUNS + 1; i++) {
+                    // Manually call gc before benchmarking.
                     System.gc();
                     long start = System.nanoTime();
                     ThreadedCountRunner t = new ThreadedCountRunner(nr_threads, nr_threads, cm);
                     long stop = System.nanoTime();
                     // First run is only warmup.
-                    if (i > 0)
-                        runtime += stop - start;
+                    if (i > 0) {
+                        runtimes[cm.ordinal()] += stop - start;
+                    }
                 }
-                System.out.printf("%d\t%s\t%f\n", nr_threads, cm, runtime/NR_RUNS/1000000.f);
+            }
+            long referenceRuntimePerRun = runtimes[ComputationMode.NAIVE.ordinal()] / NR_RUNS;
+            for (ComputationMode cm: ComputationMode.values()) {
+                long runtimePerRun = runtimes[cm.ordinal()] / NR_RUNS;
+                float runtimeMs = runtimePerRun  / 1000000.f;
+                float speedup = (float) referenceRuntimePerRun / runtimePerRun;
+                System.out.printf("%d\t%s\t%f\t%f\n", nr_threads, cm, runtimeMs, speedup);
             }
         }
     }
