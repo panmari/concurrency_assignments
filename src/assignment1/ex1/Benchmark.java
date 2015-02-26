ccd71ac0657ba2b5a08dc21cab07eb22cc94a40b
panmari <smmuzi@gmail.com>
2015-02-26 11:32:11 +0100

    Smarter benchmarking with multiple runs/warmup.

--- src/assignment1/ex1/Benchmark.java
+++ src/assignment1/ex1/Benchmark.java
@@ -1,17 +1,27 @@
 package assignment1.ex1;
 
+// Computes the mean runtime of five runs for different locking methods and different number of threads.
 public class Benchmark {
 
+    private static int NR_RUNS = 5;
+
     public static void main(String[] args) throws InterruptedException {
+        //int[] different_nr_threads = new int[] {8, 4, 2, 1};
         int[] different_nr_threads = new int[] {1, 2, 4, 8};
         for (int nr_threads: different_nr_threads) {
+            long runtime = 0L;
             for (ComputationMode cm: ComputationMode.values()) {
                 // Manually call gc before benchmarking.
-                System.gc();
-                long start = System.currentTimeMillis();
-                ThreadedCountRunner t = new ThreadedCountRunner(nr_threads, nr_threads, cm);
-                long stop = System.currentTimeMillis();
-                System.out.printf("%d\t%s\t%d\t%d\n", nr_threads, cm, t.counter.count, stop - start);
+                for (int i = 0; i < NR_RUNS + 1; i++) {
+                    System.gc();
+                    long start = System.nanoTime();
+                    ThreadedCountRunner t = new ThreadedCountRunner(nr_threads, nr_threads, cm);
+                    long stop = System.nanoTime();
+                    // First run is only warmup.
+                    if (i > 0)
+                        runtime += stop - start;
+                }
+                System.out.printf("%d\t%s\t%f\n", nr_threads, cm, runtime/NR_RUNS/1000000.f);
             }
         }
     }
