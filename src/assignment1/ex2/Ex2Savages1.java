8eef6a33e419ec8c55f4a45b1e01e28bf3ef08d9
panmari <smmuzi@gmail.com>
2015-02-27 14:07:24 +0100

    More blocking for correct behaviour
    Only one savage is allowed at the pot at any time.

--- src/assignment1/ex2/Ex2Savages1.java
+++ src/assignment1/ex2/Ex2Savages1.java
@@ -6,46 +6,63 @@ import java.util.ArrayList;
 
 public class Ex2Savages1 {
 
-    private static final int NR_SAVAGES = 20;
+    private static final int NR_SAVAGES = 101;
 
     public static void main(String[] args) throws InterruptedException {
-        ArrayList<Thread> threads = new ArrayList<Thread>(NR_SAVAGES);
+        ArrayList<Thread> savageThreads = new ArrayList<Thread>(NR_SAVAGES);
 
         Pot pot = new Pot();
         Cook cook = new Cook(pot);
+        Thread cookThread = new Thread(cook);
         for (int i = 0; i < NR_SAVAGES; i++) {
-            threads.add(new Thread(new Savage(pot, cook)));
+            savageThreads.add(new Thread(new Savage(pot, cook)));
         }
 
-        for (Thread t: threads) {
+        // Start threads:
+        cookThread.start();
+        for (Thread t : savageThreads) {
             t.start();
         }
-        for (Thread t: threads) {
+        for (Thread t : savageThreads) {
             t.join();
         }
+        // Stop cook:
+        cook.quitJob();
+        cookThread.join();
+
+        System.out.printf("Cook had to refill %d times.\n", cook.nrRefills);
     }
 }
 
 class Savage implements Runnable {
     private final Cook cook;
     private final Pot pot;
+    private boolean hungry;
 
     public Savage(Pot pot, Cook cook) {
+        this.hungry = true;
         this.pot = pot;
         this.cook = cook;
     }
 
     @Override
     public void run() {
-        eat();
+        while(hungry) {
+            // Only one savage may access the pot at any time.
+            synchronized (pot) {
+                eat();
+            }
+        }
     }
 
-    public synchronized void eat() {
+    public void eat() {
         try {
             pot.takePortion();
-        } catch (RuntimeException e) {
-            cook.run();
-            eat();
+            hungry = false;
+        } catch (PotException e) {
+            cook.inform();
+            // Wait while cook is cooking.
+            while(cook.isCooking()) {}
         }
     }
 }
@@ -76,13 +93,38 @@ class PotException extends RuntimeException {
 
 class Cook implements Runnable {
 
+    int nrRefills = 0;
     private final Pot pot;
+    private boolean doRefill;
+    private boolean stopRefilling;
 
     public Cook(Pot pot) {
         this.pot = pot;
     }
 
+    public void inform() {
+        doRefill = true;
+    }
+
+    public boolean isCooking() {
+        return doRefill;
+    }
+
+    // Can be used once all the savages have eaten enough.
+    public void quitJob() {
+        stopRefilling = true;
+    }
+
     @Override
     public void run() {
-        pot.fill();
-    }}
+        while (true) {
+            if (doRefill) {
+                pot.fill();
+                nrRefills++;
+                doRefill = false;
+            }
+            if (stopRefilling)
+                break;
+        }
+    }
+}
